fda6ec3f3c4c6ad96c13588bc479b562e846514f
Alejandro Soto <alejandro@34project.org>
2022-05-20 13:25:50 -0600

    Add /api/identity/login stub

---
+++ web/Controllers/IdentityController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class IdentityController : ControllerBase
+{
+    [HttpPost("login")]
+    public IResult Login(string username, string password)
+    {
+        var ok = username == "foo" && password == "bar";
+        return ok ? Results.Ok(new { id = 69 }) : Results.Unauthorized();
+	}
+}
