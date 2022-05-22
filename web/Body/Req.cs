342ab575517f8aa9766ffd299dfc6cb0ccca0611
Alejandro Soto <alejandro@34project.org>
2022-05-22 14:02:05 -0600

    Add password field to Req.NewUser

--- web/Body/Req.cs
+++ web/Body/Req.cs
@@ -23,6 +23,8 @@ public record NewUser
     [Required]
     public string Username { get; set; } = null!;
     [Required]
+    public string Password { get; set; } = null!;
+    [Required]
     public string FirstName { get; set; } = null!;
     [Required]
     public string LastName { get; set; } = null!;
