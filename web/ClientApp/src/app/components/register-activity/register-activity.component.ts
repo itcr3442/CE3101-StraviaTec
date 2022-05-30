6668386510a7140173ac5de3ac7f701fb7186104
Ignacio Vargas <ignaevc@gmail.com>
2022-05-30 00:37:37 -0600

    Got leaflet-gpx map working on registery activity

--- web/ClientApp/src/app/components/register-activity/register-activity.component.ts
+++ web/ClientApp/src/app/components/register-activity/register-activity.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { ActivityType } from 'src/app/constants/activity.constants';
+import { circle, latLng, Layer, tileLayer, map as leafMap, Map as LeafMap, LayerEvent, GPX } from 'leaflet';
+import * as L from 'leaflet-gpx';
+import { map } from 'jquery';
 
 @Component({
   selector: 'app-register-activity',
@@ -10,6 +13,15 @@ import { ActivityType } from 'src/app/constants/activity.constants';
   styleUrls: ['./register-activity.component.css']
 })
 export class RegisterActivityComponent implements OnInit {
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '...' })
+    ],
+    zoom: 15,
+    center: latLng(9.855319, -83.910799)
+  };
+
 
   registerForm = new FormGroup({
     startDate: new FormControl('', [Validators.required]),
@@ -30,7 +42,11 @@ export class RegisterActivityComponent implements OnInit {
   message: string = "";
   maxDate: string;
   minDate: string = '1900-01-01'
+
+  mapReference: LeafMap | null = null
+
   gpxURL: string | null = null;
+  gpxLayer: Layer[] = [];
 
   constructor() {
     let today = new Date()
@@ -42,6 +58,10 @@ export class RegisterActivityComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onMapReady(map: LeafMap) {
+    this.mapReference = map
   }
 
   onSubmit() {
@@ -57,10 +77,31 @@ export class RegisterActivityComponent implements OnInit {
   }
 
   upload(files: FileList) {
+    if (this.gpxURL) {
+      URL.revokeObjectURL(this.gpxURL)
+    }
+
+    this.gpxLayer = []
+
     this.gpxURL = URL.createObjectURL(files[0]);
-    console.log("gpxfiles:", files)
-    console.log("gpx:", files[0])
-    console.log("gpxURL:", this.gpxURL)
+
+    if (this.mapReference) {
+      let gpxMap = this.mapReference;
+
+      // @ts-ignore
+      let layer = new L.GPX(this.gpxURL, {
+        async: true,
+        marker_options: {
+          startIconUrl: 'assets/gpx-icons/pin-icon-start.png',
+          endIconUrl: 'assets/gpx-icons/pin-icon-end.png',
+          shadowUrl: 'assets/gpx-icons/pin-shadow.png'
+        }
+      }).on('loaded', function (e: LayerEvent) {
+        gpxMap.fitBounds(e.target.getBounds());
+      })
+
+      this.gpxLayer.push(layer)
+    }
   }
 
 }
