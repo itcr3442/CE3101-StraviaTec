c78e699eb87ab2fa92330399836cc0dc3fb46144
Alejandro Soto <alejandro@34project.org>
2022-06-01 11:05:52 -0600

    Actualiza rqlite.nix

--- taller/rqlite.nix
+++ taller/rqlite.nix
@@ -1,17 +1,20 @@
-{ lib, buildGoModule, fetchFromGitHub }:
+{ pkgs ? import <nixpkgs> {} }:
 let
-  version = "7.5.0";
-in buildGoModule {
-  pname = "rqlite";
-  inherit version;
-
-  src = fetchFromGitHub {
-    owner = "rqlite";
-    repo = "rqlite";
-    rev = "v${version}";
-    sha256 = "0hi5kq8w26i8azlcxy750zmbciga6l5n090ir261n00djigm5m59";
-  };
-
-  vendorSha256 = "sha256-YT1nK1vFmNCRJyWOiQhSJr83qW8uxkHXCZ81/Ch6qpg=";
-  doCheck = false;
-}
+  drv = { lib, buildGoModule, fetchFromGitHub }:
+    let
+      version = "7.5.0";
+    in buildGoModule {
+      pname = "rqlite";
+      inherit version;
+    
+      src = fetchFromGitHub {
+        owner = "rqlite";
+        repo = "rqlite";
+        rev = "v${version}";
+        sha256 = "0hi5kq8w26i8azlcxy750zmbciga6l5n090ir261n00djigm5m59";
+      };
+    
+      vendorSha256 = "sha256-YT1nK1vFmNCRJyWOiQhSJr83qW8uxkHXCZ81/Ch6qpg=";
+      doCheck = false;
+    };
+in pkgs.callPackage drv {}
