3a5256c3a84f262de9bbc36137adc48c3f46b052
Ignacio Vargas <ignaevc@gmail.com>
2022-06-02 13:55:30 -0600

    Made labels bigger and added buttons to home to check categories/race/challenges

--- web/ClientApp/src/app/components/home/home.component.ts
+++ web/ClientApp/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RepositoryService } from 'src/app/services/repository.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(private repo: RepositoryService) { }
+
   test() {
     console.log("test")
+    // this.repo.getData('Categories/Available').subscribe((resp: HttpResponse))
+  }
+
+  categories() {
+    this.repo.getData<Array<number>>('Categories/Available').subscribe((resp: HttpResponse<Array<number>>) =>
+      console.log("Categories Available:", resp.body))
+  }
+  races() {
+    this.repo.getData<Array<number>>('Races/Available').subscribe((resp: HttpResponse<Array<number>>) =>
+      console.log("Races Available:", resp.body))
+  }
+  challenges() {
+    this.repo.getData<Array<number>>('Challenges/Available').subscribe((resp: HttpResponse<Array<number>>) =>
+      console.log("Challenges Available:", resp.body))
   }
 }
