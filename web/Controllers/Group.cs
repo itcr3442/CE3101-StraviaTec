76c2e4cdcb6f7ab71f30fcade549cb5f6833a4c3
Alejandro Soto <alejandro@34project.org>
2022-06-03 19:16:00 -0600

    Implement POST /api/groups

--- web/Controllers/Group.cs
+++ web/Controllers/Group.cs
@@ -24,14 +24,36 @@ public class GroupController : ControllerBase
     [Produces(MediaTypeNames.Application.Json)]
     [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Resp.Ref))]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public ActionResult New(Req.NewGroup req)
+    public async Task<ActionResult> New(Req.NewGroup req)
     {
-        if (Random.Shared.Next(2) == 0)
+        int id;
+        using (var txn = _db.Txn())
         {
-            return BadRequest();
+            string query = @"
+                INSERT INTO groups(name, admin)
+                OUTPUT INSERTED.ID
+                VALUES(@name, @admin)
+                ";
+
+            using (var cmd = txn.Cmd(query))
+            {
+                id = cmd.Param("name", req.Name).Param("admin", req.Admin).InsertId();
+            }
+
+            query = @"
+                INSERT INTO group_members(group_id, member)
+                VALUES(@id, @admin)
+                ";
+
+            using (var cmd = txn.Cmd(query))
+            {
+                await cmd.Param("id", id).Param("admin", req.Admin).Exec();
+            }
+
+            txn.Commit();
         }
 
-        return CreatedAtAction(nameof(Get), new { id = 69 }, new Resp.Ref(69));
+        return CreatedAtAction(nameof(Get), new { id = id }, new Resp.Ref(id));
     }
 
     [HttpPatch("{id}")]
